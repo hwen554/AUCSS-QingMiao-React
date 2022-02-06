8a45dd5aeaa7ef838e825c6ad7fec1b637e5cc0c
hwen554 <78581470+hwen554@users.noreply.github.com>
2022-02-07 01:33:29 +1300

    update

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -3,7 +3,7 @@ import '../../App.css';
 
 export default function SignUp() {
   return (
-    <video src='/videos/video-3.mp4' autoPlay loop unmuted />
+    <video src='/videos/video-1.mp4' autoPlay loop unmuted />
     
   );
 }
