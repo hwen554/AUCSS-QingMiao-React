56dc62dff414e4b2837aa14d379466a92e242aba
hwen554 <78581470+hwen554@users.noreply.github.com>
2022-02-11 20:29:53 +1300

    update

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -3,7 +3,28 @@ import '../../App.css';
 
 export default function SignUp() {
   return (
+    <>
     <video src='/videos/video-1.mp4' autoPlay loop unmuted />
+    <div className='container'>
+      <div className='title'>Registration</div>
+      <div className='content'>
+        <form action='#'>
+          <div className='member-details'>
+            <div className='input-box'>
+              <span className='details'>Full Name</span>
+              <input type='text' placeholder='Enter your name' required></input>
+            </div>
+            <div className='input-box'>
+              <span className='details'>Email</span>
+              <input type='text' placeholder='Enter your email' required></input>
+            </div>
+            
+          </div>
+        </form>
+      </div>
+    </div>
+    </>
+    
     
   );
 }
